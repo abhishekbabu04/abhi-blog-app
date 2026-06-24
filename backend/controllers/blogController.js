75e8847833ce6640b1300fb1f5c2ecd8d1489f9c
abhishek <babuabhishek920@gmail.com>
2026-06-24 20:01:45 +0530

    Fixed validation error

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -6,67 +6,73 @@ import { v2 as cloudinary } from "cloudinary";
 
 export const createBlog = async (req, res) => {
 
-    try {
-        if (!req.files || !req.files.blogImage) {
-            return res.status(400).json({ message: "Blog image  is required" });
-        }
+  try {
+    if (!req.files || !req.files.blogImage) {
+      return res.status(400).json({ message: "Blog image  is required" });
+    }
 
-        const { blogImage } = req.files;
+    const { blogImage } = req.files;
 
-        const allowedFormats = ["image/jpg", "image/png", "image/jpeg", "image/webp"];
-        if (!allowedFormats.includes(blogImage.mimetype)) {
-            return res.status(400).json({ message: "Invalid image format" });
-        }
+    const allowedFormats = ["image/jpg", "image/png", "image/jpeg", "image/webp"];
+    if (!allowedFormats.includes(blogImage.mimetype)) {
+      return res.status(400).json({ message: "Invalid image format" });
+    }
 
-        const { title, category,  about } = req.body;
+    const { title, category, about } = req.body;
 
-        if (!title || !category || !about  ) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
+    if (!title || !category || !about) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
-         const adminName = req.user.name;
-         const adminPhoto = req.user.photo?.url;
-         const createdBy = req.user._id;
+    const adminName = req.user.name;
+    const adminPhoto = req.user.photo?.url;
+    const createdBy = req.user._id;
 
-        const cloudinaryResponse = await cloudinary.uploader.upload(
-            blogImage.tempFilePath
-        );
+    const cloudinaryResponse = await cloudinary.uploader.upload(
+      blogImage.tempFilePath
+    );
 
-        if (!cloudinaryResponse || cloudinaryResponse.error) {
-            return res.status(500).json({error: "Image upload failed" });
-        }
-
-        
-        const blogData = ({
-            title,
-            category,
-            about,
-            adminName,
-            adminPhoto,
-            createdBy,
-            blogImage: {
-                public_id: cloudinaryResponse.public_id,
-                url: cloudinaryResponse.secure_url
-            }
-        });
-
-      const blog=  await Blog.create(blogData);
-         
-            
-            res.status(201).json({ message: "Blog created successfully", blog });
-        
-    } catch (error) {
-          console.log(error)
-           res.status(500).json({ error:"Internal server error" });
+    if (!cloudinaryResponse || cloudinaryResponse.error) {
+      return res.status(500).json({ error: "Image upload failed" });
     }
 
 
+    const blogData = ({
+      title,
+      category,
+      about,
+      adminName,
+      adminPhoto,
+      createdBy,
+      blogImage: {
+        public_id: cloudinaryResponse.public_id,
+        url: cloudinaryResponse.secure_url
+      }
+    });
+
+    const blog = await Blog.create(blogData);
+
+
+    res.status(201).json({ message: "Blog created successfully", blog });
+
+  }  catch (error) {
+  console.log(error);
+
+  if (error.name === "ValidationError") {
+    const message = Object.values(error.errors)[0].message;
+    return res.status(400).json({ message });
+  }
+
+  res.status(500).json({ error: "Internal server error" });
+
+
+  }
 };
 
 
 export const deleteBlog = async (req, res) => {
   try {
-    
+
     const { id } = req.params;
 
     const blog = await Blog.findById(id);
@@ -85,30 +91,29 @@ export const deleteBlog = async (req, res) => {
 };
 
 
-export const getAllBlogs=async (req,res)=>{
-const allBlogs=await Blog.find()
-res.status(200).json(allBlogs);
+export const getAllBlogs = async (req, res) => {
+  const allBlogs = await Blog.find()
+  res.status(200).json(allBlogs);
 };
 
-export const getSingleBlog = async(req,res)=>{
-    const {id} =req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-        return res.status(404).json({ message: "Invalid Blog id" });
-    }
-    const blog =await Blog.findById(id);
-    if (!blog) {
-      return res.status(404).json({ message: "Blog not found" });
-    }
-    res.status(200).json(blog)
-     
+export const getSingleBlog = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "Invalid Blog id" });
+  }
+  const blog = await Blog.findById(id);
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+  res.status(200).json(blog)
+
 }
 
 
-export const getMyBlogs = async(req,res)=>{
-    const createdBy=req.user._id;
-    const myBlogs=await Blog.find({createdBy});
-    res.status(200).json(myBlogs);
+export const getMyBlogs = async (req, res) => {
+  const createdBy = req.user._id;
+  const myBlogs = await Blog.find({ createdBy });
+  res.status(200).json(myBlogs);
 };
 
 export const updateBlog = async (req, res) => {
